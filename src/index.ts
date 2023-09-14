3ea82dec237816023ccee758513503f6656390ff
duleendra <shashimald@gmail.com>
2023-09-14 12:49:17 +0800

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import {Config} from "./config/config";
 import {InfraPipeline} from "./stacks/infra-pipeline/infra-pipeline";
 import {ApplicationStackStage} from "./stacks/infra-pipeline/application-stack-stage";
 
+
 const app = new cdk.App();
 // const env = app.node.tryGetContext("env");
 //
 // if (!env) {
 //     throw new Error("Env not found");
 // }
+
+const deployEnv = process.env.DEPLOY_ENV || "dev"
+
+console.log(deployEnv)
 
 const config = Config.getConfig("dev");
 const appName = config.appName.toLowerCase();
@@ -26,59 +31,3 @@ const stage = new ApplicationStackStage(app, 'ApplicationStage', {
 })
 
 pipeline.pipeline.addStage(stage)
-
-// const dnsStage = new DnsStackStage(app, `${appName}-dns-stack-stage`, {
-//     env: appEnv,
-//     appName: appName,
-//     zoneName: config.domainName
-// })
-//
-// const cloudfrontCertificateStage =new CloudfrontCertificateStackStage(app, `${appName}-acm-stack`, {
-//     env: {account: config.account, region: "us-east-1"},
-//     appName: config.appName,
-//     domainName: config.domainName,
-//     hostedZone: dnsStage.getHostedZone()
-// });
-//
-// const staticSiteStackStage = new StaticSiteStackStage(app, `${appName}-static-site-stack`, {
-//     env: appEnv,
-//     appName: config.appName,
-//     bucketName: config.bucketName,
-//     zone: dnsStage.getHostedZone(),
-//     certificate: cloudfrontCertificateStage.getCloudfrontCertificate(),
-//     domainName: config.domainName,
-//     websiteErrorDocument: config.websiteErrorDocument,
-//     websiteIndexDocument: config.websiteIndexDocument
-// })
-//
-// pipeline.pipeline.addStage(dnsStage);
-// pipeline.pipeline.addStage(cloudfrontCertificateStage);
-// pipeline.pipeline.addStage(staticSiteStackStage);
-
-
-
-// const publicHostedZone = new PublicHostedZone(app, `${appName}-public-hosted-zone-stack`, {
-//     env: appEnv,
-//     zoneName: config.domainName
-// });
-
-// const cloudfrontCertificate = new CloudfrontCertificate(app, `${appName}-acm-stack`, {
-//     env: {account: config.account, region: "us-east-1"},
-//     crossRegionReferences: true,
-//     appName: config.appName,
-//     domainName: config.domainName,
-//     hostedZone: dnsStage.getHostedZone()
-//
-// });
-
-// const staticSiteStack = new StaticSite(app, `${appName}-static-site-stack`, {
-//     env: appEnv,
-//     crossRegionReferences: true,
-//     appName: config.appName,
-//     bucketName: config.bucketName,
-//     zone: dnsStage.getHostedZone(),
-//     certificate: cloudfrontCertificateStage.getCloudfrontCertificate(),
-//     domainName: config.domainName,
-//     websiteErrorDocument: config.websiteErrorDocument,
-//     websiteIndexDocument: config.websiteIndexDocument
-// });
